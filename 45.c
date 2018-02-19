3ed6f726de8d20c348b66d335c3d5ec00145a91e
jaganjaga <35915157+jaganjaga@users.noreply.github.com>
2018-02-19 13:39:08 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int n,c=0;
+   printf("Enter an integer: ");
+   scanf("%d", &n);
+   while(n!=0)
+   {
+   n /= 10;
+   ++c;
+    }
+    printf("Number of digits: %d", c);
+    getch();
+}
