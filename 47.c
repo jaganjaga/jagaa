19a3b161c1ef4f3f0447c7cb81244fa7a520ed81
jaganjaga <35915157+jaganjaga@users.noreply.github.com>
2018-02-21 14:07:19 +0530

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i,min,max;
+int b[10];
+clrscr();
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+scanf("%d",&b[i]);
+}
+min=max=b[0];
+for(i=0;i<a;i++)
+{
+if(b[i]<min)
+{
+min=b[i];
+}
+}
+printf("%d\t",min);
+for(i=0;i<a;i++)
+{
+if(b[i]>max)
+{
+max=b[i];
+}
+}
+printf("%d",max);
+}
