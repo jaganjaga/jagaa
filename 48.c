53268144b3dc50ef7a85ce1c51f1d0db938467c5
jaganjaga <35915157+jaganjaga@users.noreply.github.com>
2018-02-21 14:11:49 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int i, n;
+    float arr[100];
+    printf("Enter total number of elements(1 to 100): ");
+    {
+       printf("Enter Number %d: ", i+1);
+       scanf("%f", &arr[i]);
+    }
+    for(i = 1; i < n; ++
+       if(arr[0] < arr[i])
+           arr[0] = arr[i];
+    }
+    printf("Largest element = %.2f", arr[0]);
+    getch();
+}
