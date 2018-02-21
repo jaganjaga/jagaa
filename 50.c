b946e5f69adf402de3f1f7edf23da1ae57554ab1
jaganjaga <35915157+jaganjaga@users.noreply.github.com>
2018-02-21 14:20:10 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int i, n;
+    float arr[100];
+    printf("Enter total number of elements(1 to 100): ");
+       printf("Enter Number %d: ", i+1);
+       scanf("%f", &arr[i]);
+    for(i = 1; i < n; ++
+    printf("Largest element = %.2f", arr[0]);
+    getch();
+}
