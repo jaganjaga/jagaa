9566f4f4dcfb074bdb83d55b3ae4bbd1a6ef243e
jaganjaga <35915157+jaganjaga@users.noreply.github.com>
2018-03-14 15:56:56 +0530

    Create 97.c

---
+++ 97.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)  
+{
+int n,remainder,reverse=0;
+ 
+int result;
+ 
+printf("\n enter the number:");
+ 
+scanf("%d",&n); 
+while(n!=0)
+ 
+{
+ 
+remainder=n%10;
+reverse=reverse*10+remainder;
+ 
+n=n/10;
+ 
+}
+ 
+printf("\n%d",reverse);
+getch();
+ 
+}
